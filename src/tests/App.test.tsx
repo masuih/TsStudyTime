0a2939dc89b097f779400f33c2b16d4643deeb27
Crawd <test@example.com>
2024-11-10 11:32:12 +0900

    テスト時にimport周りが動かない事象を修正

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,10 +1,20 @@
-import {render, screen} from '@testing-library/react'
-import '@testing-library/jest-dom'
-import App from "../App"
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import App from "../App";
 
-describe('てすと', () => {
-  test('てすと', () => {
-    render(<App />)
+window.matchMedia =
+  window.matchMedia ||
+  function () {
+    return {
+      matches: false,
+      addListener: function () {},
+      removeListener: function () {},
+    };
+  };
+
+describe("てすと", () => {
+  test("てすと", () => {
+    render(<App />);
     expect(screen.getByText("Test")).toBeInTheDocument();
-  })
-})
+  });
+});
