7d3f97d72d156ed002d7fe8ca94786cef7888a68
Crawd <test@example.com>
2024-11-10 09:38:20 +0900

    Chakra UIの導入 エイリアスの設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import { Provider } from "@/components/ui/provider";
+import { Button } from "@/components/ui/button";
+
 function App() {
   return (
-    <>
+    <Provider>
       <div>Test</div>
-    </>
+      <Button colorPalette="teal">TestButton</Button>
+    </Provider>
   );
 }
 
